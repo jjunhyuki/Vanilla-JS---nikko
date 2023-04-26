a02e17ae2125c661d5eec7bd837ab3bf7122da8b
jjunhyuki <yjs99004@naver.com>
2023-04-26 14:06:50 +0900

    const

--- welecome to javascript/script.js
+++ welecome to javascript/script.js
@@ -1,1 +1,16 @@
-alert("hi");
+//can't change
+const a = 10;
+const b = 6;
+const myName = "junhyuki";
+
+//can change
+let yourName = "hani";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("my name is " + myName);
+console.log("your name is " + myName);
+
+yourName = "hjeong";
+console.log("your name is " + myName);
