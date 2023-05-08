68762118970766d13fb4eb3c61dceeee781a4806
jjunhyuki <91825771+jjunhyuki@users.noreply.github.com>
2023-05-08 20:45:58 +0900

    Add files via upload

---
+++ welecome to javascript/chapter5/1.intervals&&timeout/js/clock.js
@@ -0,0 +1,9 @@
+const clock = document.querySelector("h2#clock");
+
+function sayHello() {
+    console.log("hello");
+}
+
+//함수를 매2초 마 실행시키고 싶을때 
+//두번째 인자는 새로고침 할 시간으로 받는데 5000 => 5second
+setInterval(sayHello, 5000);
